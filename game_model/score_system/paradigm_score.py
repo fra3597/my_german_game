d88508d7b167e517c6ed79d0df0447583d931cec
Fra <france.poltro@gmail.com>
2023-11-19 17:05:38 +0100

    Implemented features: - Play again for paradigm mode - Show score in paradigm summary

--- game_model/score_system/paradigm_score.py
+++ game_model/score_system/paradigm_score.py
@@ -2,9 +2,13 @@ class ParadigmScore:
     def __init__(self):
         super().__init__()
 
-        self.present_counter = 0
-        self.präteritum_counter = 0
-        self.perfekt_counter = 0
+        self.present_partial_counter = 0
+        self.präteritum_partial_counter = 0
+        self.perfekt_partial_counter = 0
+
+        self.present_total_counter = 0
+        self.präteritum_total_counter = 0
+        self.perfekt_total_counter = 0
 
         self.present_partial_score = 0
         self.präteritum_partial_score = 0
@@ -13,3 +17,23 @@ class ParadigmScore:
         self.present_total_score = 0
         self.präteritum_total_score = 0
         self.perfekt_total_score = 0
+
+    def update_total_score(self):
+        self.present_total_score += self.present_partial_score
+        self.präteritum_total_score += self.präteritum_partial_score
+        self.perfekt_total_score += self.perfekt_partial_score
+
+    def update_total_counter(self):
+        self.present_total_counter += self.present_partial_counter
+        self.präteritum_total_counter += self.präteritum_partial_counter
+        self.perfekt_total_counter += self.perfekt_partial_counter
+
+    def reset_partial_score(self):
+        self.present_partial_score = 0
+        self.präteritum_partial_score = 0
+        self.perfekt_partial_score = 0
+
+    def reset_counter(self):
+        self.present_partial_counter = 0
+        self.präteritum_partial_counter = 0
+        self.perfekt_partial_counter = 0
